2dc410f442551c86fc4868d6360ef112e5478eeb
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-02-15 20:00:21 +0100

    ✅ Add contextLoads test

---
+++ src/test/java/com/angelocipriani/electroniccomponentwarehouse/ElectronicComponentWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelocipriani.electroniccomponentwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicComponentWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
