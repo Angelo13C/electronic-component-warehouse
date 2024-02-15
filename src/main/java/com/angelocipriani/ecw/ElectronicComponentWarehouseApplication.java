f07dc50449f60abe1b3ebdf0b32b29b2e60d5261
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-02-15 20:06:21 +0100

    ✨ Add the main application java file

---
+++ src/main/java/com/angelocipriani/ecw/ElectronicComponentWarehouseApplication.java
@@ -0,0 +1,16 @@
+package com.angelocipriani.ecw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ElectronicComponentWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicComponentWarehouseApplication.class, args);
+		
+		System.out.println("Running!");
+	}
+}
